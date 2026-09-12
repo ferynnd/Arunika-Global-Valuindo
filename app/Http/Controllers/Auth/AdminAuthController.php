9179ff404908493db94d87db35ea2c259f919197
mmdesu <mmdesu26@gmail.com>
2026-09-12 11:42:18 +0700

    update dikit

--- app/Http/Controllers/Auth/AdminAuthController.php
+++ app/Http/Controllers/Auth/AdminAuthController.php
@@ -77,6 +77,6 @@ class AdminAuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect()->route('admin.login');
     }
 }
